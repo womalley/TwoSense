3e2df832581fc3c000a0200afa13e7b1f13b6042
womalley <womalley1495@gmail.com>
2018-04-13 11:50:13 -0400

    Switched login from JS to HTML

--- public/app.js
+++ public/app.js
@@ -1,33 +1,12 @@
 
 
-/* Given from Amazon's Firebase to connect code to database (Not my code!) */
-
-// Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyDNs60mQkp79n5tflFVEc7ihH8Kr1ayXM4",
-    authDomain: "twosense-66771.firebaseapp.com",
-    databaseURL: "https://twosense-66771.firebaseio.com",
-    projectId: "twosense-66771",
-    storageBucket: "twosense-66771.appspot.com",
-    messagingSenderId: "1043280577170"
-  };
-  firebase.initializeApp(config);
-/* End given code */
-
-/* Implementing elements for login (My code) */
-const emailIn = document.getElementById('emailIn');
-const passwordIn = document.getElementById('passwordIn');
-const registerButton = document.getElementById('registerButton');
-const loginButton = document.getElementById('loginButton');
-const logoutButton = document.getElementById('logoutButton');
-
 /* Create login on click event */
-loginButton.addEventListener('click', e => {
+function loginButtonCheck() {
 
-    // set email and password vars by user input
-    const email = emailIn.value;
-    const password = passwordIn.value;
-    
+    // grab user inputs from login page
+    const email = document.getElementById("emailIn").value;
+    const password = document.getElementById("passwordIn").value;
+
     // firebase auth
     const auth = firebase.auth();
 
@@ -37,13 +16,13 @@ loginButton.addEventListener('click', e => {
       console.log(e.message);
       alert("The email you entered does not belong to an account. Please check your email and password.");
     }); // login failed (user credentials wrong or user does not exist)
-});
+}
 
-registerButton.addEventListener('click', e => {
+function registerButtonCheck() {
 
   // set email and password vars by user input
-  const email = emailIn.value;
-  const password = passwordIn.value;
+  const email = document.getElementById("emailIn").value;
+  const password = document.getElementById("passwordIn").value;
   
   // firebase auth
   const auth = firebase.auth();
@@ -61,10 +40,10 @@ registerButton.addEventListener('click', e => {
     if (firebaseUser) {
       console.log(firebaseUser);
       console.log("User is logged in!");
+      
     }
     else {
       console.log("User is not logged in!");
     }
   });
-
-});
+}
