c7763578b7e4d74fb8bdacb5019bd86f156bd7e1
womalley <womalley1495@gmail.com>
2018-04-14 00:09:36 -0400

    Refactored Login, routed login to home, started side nav bar

--- public/app.js
+++ public/app.js
@@ -7,15 +7,14 @@ function loginButtonCheck() {
     const email = document.getElementById("emailIn").value;
     const password = document.getElementById("passwordIn").value;
 
-    // firebase auth
-    const auth = firebase.auth();
-
     // sign in with email and password
-    const loginAttempt = auth.signInWithEmailAndPassword(email, password); // login successful
+    const loginAttempt = firebase.auth().signInWithEmailAndPassword(email, password); // login successful
     loginAttempt.catch(e => {
-      console.log(e.message);
       alert("The email you entered does not belong to an account. Please check your email and password.");
-    }); // login failed (user credentials wrong or user does not exist)
+      window.location.replace("");
+    });
+
+    //window.location.replace("home.html");
 }
 
 function registerButtonCheck() {
@@ -30,20 +29,27 @@ function registerButtonCheck() {
   // register with email and password
   const registerAttempt = auth.createUserWithEmailAndPassword(email, password); // should pass (unless user exists or email doesn't exist)
   registerAttempt.catch(e => {
+    window.location.replace("");
     console.log(e.message);
     alert("The email you entered is already registered or does not exist.");
   });
+}
 
   // log user information
   firebase.auth().onAuthStateChanged(firebaseUser => {
 
     if (firebaseUser) {
       console.log(firebaseUser);
+      window.location.replace("home.html");
       console.log("User is logged in!");
-      
     }
     else {
       console.log("User is not logged in!");
     }
   });
+
+
+function logout() {
+  firebase.auth().signOut();
+  window.location.replace("index.html");
 }
