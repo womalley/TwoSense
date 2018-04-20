b8575fdd4749b9b410c89d831b96866edb983399
Adhitya Vadivel <rangerboy123@gmail.com>
2018-04-20 05:10:52 -0400

    Updated audio controls for hearingTest

--- hearingTest.js
+++ hearingTest.js
@@ -4,43 +4,39 @@ var numTrue = 0;
 var sounds = [
     ["./Tests/Hearing/100hz.mp3"],
     ["./Tests/Hearing/500hz.mp3"],
-    ["./Tests/Hearing/5000hz.mp3"]
+    ["./Tests/Hearing/1000hz.mp3"],
+    ["./Tests/Hearing/2000hz.mp3"],
+    ["./Tests/Hearing/3000hz.mp3"],
+    ["./Tests/Hearing/5000hz.mp3"],
+    ["./Tests/Hearing/7000hz.mp3"],
+    ["./Tests/Hearing/20hz.mp3"]
 ];
 
 function nextSound(response) {
+
     if (soundNum < (sounds.length)) {
       if (response.toLowerCase() === "yes")
         numTrue++;
     }
-
-    // TODO: change to radial buttons
-
-    //console.log("soundNum: " + soundNum);
-
     soundNum++;
 
-    // make sure test has not reached the end
     if (soundNum < (sounds.length)) {
+        
+        var audio = document.getElementById('audioPlayer');
+        if (audio.paused) {
+            audio.play();
+        }else{
+            audio.pause();
+            audio.currentTime = 0
+        }
+        
+        document.getElementById('audioPlayer').src = document.getElementById('soundFile').src = sounds[soundNum];
 
-          document.getElementById('soundFile').src = sounds[soundNum];
-
-        //console.log("Next color blind test check");
-    }
-    else {
-      // exit test
-      //console.log("Test is over");
-
-      //compare answer key array with user input array
-
-      //go to page for showing users score?
-      //window.location.replace("topScores.html");
-
-      // reset counter
+    } else {
       soundNum = 0;
     }  
 
     onload = function() {
         document.getElementById('soundFile').src = sounds[0];
     }
-
 }
